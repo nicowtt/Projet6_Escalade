a706614cd6383543d64f05cdca5af24d3f40fc71
nicow <nicobod31@gmail.com>
2019-04-23 12:03:52 +0200

    top web page updated
    navbar added
    .css is ok

---
+++ escalade/escalade-webapp/src/main/java/com/ocr/nicolas/controllers/BeginController.java
@@ -0,0 +1,19 @@
+package com.ocr.nicolas.controllers;
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BeginController {
+
+    static final Log logger = LogFactory.getLog(BeginController.class);
+
+    @RequestMapping(value="/index")
+    public String index(Model model) {
+        return "home_Escalade";
+    }
+}
